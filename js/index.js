53740411827656369e8258c2721ebd9217c44d51
Nana Pisanko <bonjozzy@gmail.com>
2023-09-28 21:33:44 +0300

    feat: start every block

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 import '../assets/scss/fonts.scss';
 import '../assets/scss/main.scss';
+
 import AppData from "./data/appData.js";
-import Clocks from "./classes/Clocks.js";
-import basicActions from "./actions/commonFunctions.js";
 import App from "./classes/App.js";
+import basicActions from "./actions/commonFunctions.js";
+
+import Player from './classes/Player.js';
+import tracks from "./data/tracks.js";
 import Weather from './classes/Weather.js';
-import Background from "./classes/Background.js";
-/*import Player from './classes/Player.js';*/
+import ClocksBackground from "./classes/Clocks&Background.js";
+import weather from "./classes/Weather.js";
+import Quotes from "./classes/Quotes.js";
+import Settings from "./classes/Settings.js";
+import ToDo from "./classes/ToDo.js";
 
 
 
@@ -15,17 +21,26 @@ const volume = localStorage.getItem('volume') || 30;
 const lang = localStorage.getItem('lang') || 'en';
 const name = localStorage.getItem('name') || '';
 const city = localStorage.getItem('city') || 'Minsk';
+const weatherAPIKey = '1e86aeecc6661092d35f019637c03153';
+const quotesUrl = '';
+const quotesAPIKey = '';
+const quotesArray = [];
+const tasksArray = [];
 
-const Application = new App(lang, name, city, null, null, Clocks, AppData)
+const Application = new App(lang, name, city, null, null, ClocksBackground, AppData)
 /*Application.startApp();*/
 Application.setProperty = ['basicActions', basicActions];
 Application.setProperty = ['Weather', Weather];
 basicActions.setObjectProperties(Application, Weather);
-console.log(Application);
-Weather.start();
-Background.startBackground();
 
-/*basicActions.clocks.getDate(lang);*/
+Player.start(tracks, volume);
+Weather.start(city, lang, weatherAPIKey);
+ClocksBackground.start(lang, name);
+Quotes.start(lang, quotesAPIKey, quotesUrl, quotesArray);
+Settings.start(lang);
+ToDo.start(lang, tasksArray);
+
+
 
 
 
