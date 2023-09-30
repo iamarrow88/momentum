682cc94408d254ac0e0905fbd4770c868f7d96e4
Nana Pisanko <bonjozzy@gmail.com>
2023-09-30 21:16:20 +0300

    feat: upgrade createElement function
    + add createDatasetName function

--- js/classes/base/Base.js
+++ js/classes/base/Base.js
@@ -32,11 +32,35 @@ export default class Base {
     }
     return HTMLElement.addEventListener(trigger, handler);
   }
+  createDatasetName(dataAttribute){
+    const datasetName = dataAttribute.split('-').slice(1);
+    let result = datasetName[0];
+    if(datasetName.length > 1){
+      for(let i = 1; i < datasetName.length; i++){
+        let chunk = datasetName[i].split('');
+        chunk[0] = chunk[0].toUpperCase();
+        chunk = chunk.join('');
+        result += chunk;
+      }
+    }
+    return result;
+  }
 
-  createElement(tag, className, parentElement) {
+  createElement(tag, className, parentElement, attributes) {
     const element = document.createElement(tag);
-    element.classList.add(className);
-    parentElement.append(element);
+    if(className) element.classList.add(className);
+    if(parentElement) parentElement.append(element);
+    if(attributes){
+      attributes.forEach( attr => {
+        const key = Object.keys(attr)[0];
+        if(key.split('-').includes('data')){
+          const datasetName = this.createDatasetName(key);
+          element.dataset[datasetName] = attr[key];
+        } else {
+          element.setAttribute(key, attr[key]);
+        }
+      })
+    }
     return element;
   }
 }
