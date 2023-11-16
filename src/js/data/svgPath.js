3a8a1e694b6a657753fc7b43624ace5431640da2
Nastya Pisanka <nana.pisanka@gmail.com>
2023-11-16 19:16:46 +0300

    feat: add dist for deploy

--- src/js/data/svgPath.js
+++
@@ -1,8 +0,0 @@
-const svgPath = {
-  play: "src/assets/svg/sprite.svg#play",
-  pause: "src/assets/svg/sprite.svg#pause",
-  volume: "src/assets/svg/sprite.svg#volume",
-  mute: "src/assets/svg/sprite.svg#mute",
-};
-
-export default svgPath;
--- src/js/data/translation/translation.js
+++
@@ -1,8 +0,0 @@
-import en from "./languages/en.js";
-import ru from "./languages/ru.js";
-
-const translate = {};
-translate.en = en;
-translate.ru = ru;
-
-export default translate;
